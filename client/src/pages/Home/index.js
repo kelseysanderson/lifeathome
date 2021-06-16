52bed4442f1ef381f87524d9ee59be7ed88272f5
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-06-16 00:25:45 -0500

    added homepage functionality and styling

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,17 +1,35 @@
-import React from 'react'
-
-/**
-* @author
-* @function Home
-**/
+import React, { useState, useEffect } from "react";
+import Banner from '../../components/HomeComponents/Banner';
+import AboutStatement from '../../components/HomeComponents/AboutStatement';
+import Stats from '../../components/HomeComponents/Stats';
+import AdditionalInfo from '../../components/HomeComponents/AdditionalInfo';
+import API from "../../utils/API.js";
+import './style.css';
 
 const Home = (props) => {
-  return(
-    <div>Home
-    
-    </div>
-   )
+  // const [homePage, setHomePage] = useState([]);
 
- }
+  // useEffect(() => {
+  //   loadHomePage();
+  // }, []);
+
+  // function loadHomePage() {
+  //   API.getHome()
+  //     .then(res =>
+  //       console.log(res)
+  //       // setHomePage(res.data),
+  //     )
+  //     .catch(err => console.log(err));
+  // };
+
+  return (
+    <div className="home-page">
+      <Banner />
+      <AboutStatement />
+      <Stats />
+      <AdditionalInfo />
+    </div>
+  )
+}
 
 export default Home
