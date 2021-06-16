1f1684f39bb48812af0e1452f353054eaef06e13
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-06-16 00:26:43 -0500

    added additional homepage components

---
+++ client/src/components/HomeComponents/Stats/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import StatsCircle from './StatsCircle'
+import './style.css';
+
+
+
+const Stats = (props) => {
+    return (
+        <div className="stats-page">
+            <StatsCircle key="" number="60" description="here is the statistics description" statsLink="google.com"/>
+        </div>
+
+    )
+}
+
+export default Stats
