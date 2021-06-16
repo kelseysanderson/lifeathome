b3b977e1792d5b63a77ecebf8acb278511cf527f
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-06-16 00:27:46 -0500

    added initial blog page and components updated app.js routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Services from './pages/Services';
+import Blog from './pages/Blog';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import './App.css';
 
 function App() {
   return (
     <Router>
         <Header />
           <Route exact path="/" component={Home} />
+          <Route exact path="/services" component={Services} />
+          <Route exact path="/blog" component={Blog} />
         <Footer />
     </Router>
   );
