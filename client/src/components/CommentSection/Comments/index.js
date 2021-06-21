1468766e9d7c0ea0cdca84c0a969e8e12f7cb0d1
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-06-21 12:43:31 -0500

    fixed Comment section rendering

--- client/src/components/CommentSection/Comments/index.js
+++ client/src/components/CommentSection/Comments/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import API from '../../../utils/API'
-import { Grid, Box } from '@material-ui/core';
 import replyIcon from '../../../assets/reply-icon.png'
 import './style.css'
 
@@ -8,24 +7,27 @@ import './style.css'
 
 const Comments = (postId) => {
   const [comments, setComments] = useState()
-  // const userId = "01a"
-  // // const avatarUrl = "https://ui-avatars.com/api/name=Riya&background=random"
-  // const name = "xyz"
-  // const signinUrl = "/signin"
-  // const signupUrl = "/signup"
-  // let count = 0
-  // comment.map(i => { count += 1; i.replies && i.replies.map(i => count += 1) })
+  const [loading, setLoading] = useState(false);
+
   console.log(postId)
 
   useEffect(() => {
     const fetchComments = async () => {
+      setLoading(true);
+
       const res = await API.getComments(`postId=${postId.postId}`);
       setComments(res.data);
+      setLoading(false);
+
     }
     fetchComments()
   }, []);
 
   console.log(comments)
+
+  if (!comments) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <>
     {comments.map(comment => (
