d7102ddc61b612e78ad3204a9131682d4191663d
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-07-08 22:59:30 -0500

    routing attempt for heroku push

--- back-end/server.js
+++ back-end/server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
