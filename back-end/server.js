4a06b31f6f559bde5c927abe449b541cf8a05252
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-07-09 12:08:50 -0500

    heroku

--- back-end/server.js
+++ back-end/server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
 
 // Add routes, both API and view
