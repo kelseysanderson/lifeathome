f617999aeb0456ba5c9bbdf8205a33704d45490e
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-07-08 22:51:06 -0500

    add path to server file

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
