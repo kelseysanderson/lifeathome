bb01861f1961346ebab54e33798d728cc5aee1c6
Eric Heltemes <e.heltemes83@gmail.com>
2021-07-14 10:52:05 -0500

    delete blogbody on update form

--- client/src/components/APIButtons/blog.js
+++ client/src/components/APIButtons/blog.js
@@ -3,13 +3,14 @@ import { BlogContext } from '../../Context/BlogContext';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
 
+//requires postId
 const Delete = (props) => {
   const { deletePost } = useContext(BlogContext);
 
   return (
-      <IconButton onClick={() => deletePost(props.postId)} style={{color:'rgb(177, 9, 9)'}}>
-        <DeleteIcon />
-      </IconButton>
+    <IconButton onClick={() => deletePost(props.postId)} style={{ color: 'rgb(177, 9, 9)' }}>
+      <DeleteIcon />
+    </IconButton>
   )
 }
 
@@ -17,10 +18,11 @@ const Submit = (props) => {
   const { postBlogData } = useContext(BlogContext);
 
   return (
-      <button onClick={postBlogData} className="green-btn">Post to Blog</button>
+    <button onClick={postBlogData} className="green-btn">Post to Blog</button>
   )
 }
 
+//requires sectionIndex, objIndex, direction, and postId
 const Reorder = (props) => {
   const { reorderBlogBody, blogCounter } = useContext(BlogContext);
 
@@ -32,15 +34,30 @@ const Reorder = (props) => {
           <button onClick={() => reorderBlogBody(props.sectionIndex, props.objIndex, props.direction, props.postId)}>{props.direction}</button>
         </>
       ) : (
-        <>
-
-        </>
+        <></>
       )}
     </>
-
   )
 }
 
-const BlogButton = {Delete, Submit, Reorder}
+//requires sectionIndex, objIndex and postId
+const DeleteBlogBody = (props) => {
+  const { deleteBlogBody, blogCounter } = useContext(BlogContext);
+  return (
+    <>
+      {blogCounter.array[props.objIndex] === 0 || blogCounter.array[props.objIndex] === undefined ? (
+        <>
+          <IconButton onClick={() => deleteBlogBody(props.sectionIndex, props.objIndex, props.postId)} style={{ color: 'rgb(177, 9, 9)' }}>
+            <DeleteIcon />
+          </IconButton>
+        </>
+      ) : (
+        <></>
+      )}
+    </>
+  )
+}
+
+const BlogButton = { Delete, Submit, Reorder, DeleteBlogBody }
 
 export default BlogButton
